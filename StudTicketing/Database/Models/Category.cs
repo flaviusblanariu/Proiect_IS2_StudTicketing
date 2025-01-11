ddcdd8db06cd30917ba5e8b762c2a72025fc9ec1
Blana <blanariuflavius1@gmail.com>
2025-01-11 13:53:59 +0200

    Commit - tabels

---
+++ StudTicketing/Database/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudTicketing.Database.Models;
+
+public partial class Category
+{
+    public int id { get; set; }
+    
+    public string Functie_persoana { get; set; } = null!;
+
+    public string Tip_tichet { get; set; } = null!;
+
+}
---
+++ StudTicketing/Database/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudTicketing.Database.Models;
+
+public partial class Ticket
+{
+    public int id { get; set; }
+
+    public string Tip_tichet { get; set; } = null!;
+
+    public string Severitate { get; set; } = null!;
+
+    public string Stadiu { get; set; } = null!;
+
+}
