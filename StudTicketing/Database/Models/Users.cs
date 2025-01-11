31fc6e019c9ffbe1d7e51d651d5bdab9b8971758
Blana <blanariuflavius1@gmail.com>
2025-01-11 13:54:58 +0200

    Commit - tabels

---
+++ StudTicketing/Database/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudTicketing.Database.Models;
+
+public partial class Users
+{
+    public int id { get; set; }
+
+    public string Nume { get; set; } = null!;
+
+    public string Prenume { get; set; } = null!;
+
+    public string Functie { get; set; } = null!;
+
+    public string Telefon { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+}
